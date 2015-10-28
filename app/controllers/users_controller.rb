6fe5ae592740a1442f8a2f6ad13af8ef907ff6ca
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-28 18:11:38 +0200

    adds user creation logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
   def new
-    
+    # nothing to do here, just render the default view
   end
 
   def create
+    user = User.new(params)
+    if user.save
+      # login and redirect to home
+    else
+      render :new
+    end
   end
+
+  private
+
+    def user_param
+      params.require(:user).permit(:email, :password, :type)
+    end
 end
