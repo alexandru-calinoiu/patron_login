6f9aff1ebd37da18bfe8caea9a0655c7c56c9adf
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-28 18:21:21 +0200

    adds basic session logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,30 @@
 class SessionsController < ApplicationController
   def new
+    # render the login scren
   end
 
   def create
+    if valid_user?(session_params)
+      # good, store the session_id in a cookie and paste it over to identify the user
+    else
+      response status: 401
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+
+  def valid_user?(session_params)
+    email = session_params[:email]
+    password = session_params[:password]
+
+    User.any?(email: email, password: password)
+  end
+
 end
