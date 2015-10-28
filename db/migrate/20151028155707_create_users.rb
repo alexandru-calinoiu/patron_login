2a96fbeede938d65da89a38a2eacea11795b1850
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-28 18:07:27 +0200

    adds validation for user

--- db/migrate/20151028155707_create_users.rb
+++ db/migrate/20151028155707_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email, length: 60
-      t.string :password, length: 60
-      t.string :type, length: 40
+      t.string :email, length: 60, null: false
+      t.string :password, length: 60, null: true
+      t.string :type, length: 40, null: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20151028155707) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
+    t.string   "email",      null: false
     t.string   "password"
     t.string   "type"
     t.datetime "created_at", null: false
